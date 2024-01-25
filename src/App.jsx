b8f421cb361ee54a8b627e25c3ee7b826cb9b3a4
DebasishKisan1 <debasish.kisan@gmail.com>
2024-01-25 17:22:55 +0530

    visible items

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import Header from './components/Layout/Header'
+import AvailableMeals from './components/Meals/AvailableMeals'
 import Summary from './components/Meals/Summary'
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <>
       <Header />
       <Summary/>
+      <AvailableMeals/>
     </>
   )
 }
